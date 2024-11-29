fdd5644e9c1c07748ca2ac8719f50a757a703706
Jay Zheng <jz2384@cornell.edu>
2024-11-29 16:45:35 -0500

    initialize project

---
+++ BookTradingApp/BookTradingApp/BookTradingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTradingAppApp.swift
+//  BookTradingApp
+//
+//  Created by Jidong Zheng on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BookTradingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
