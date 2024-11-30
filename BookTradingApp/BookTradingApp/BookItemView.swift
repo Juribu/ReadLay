cc6f3a70886457177a3e37c1344bdad420071f65
Oshin Rex <oshinrex@gmail.com>
2024-11-29 23:22:14 -0500

    3

--- BookTradingApp/BookTradingApp/BookItemView.swift
+++ BookTradingApp/BookTradingApp/BookItemView.swift
@@ -5,13 +5,4 @@
 //  Created by Jidong Zheng on 11/29/24.
 //
 
-import SwiftUI
-
-struct BookItemView: View{
-    let book: Book
-    var body: some View{
-        VStack{
-            Text("Hello")
-        }
-    }
-}
+import Foundation
